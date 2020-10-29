b44ed2339e166fd3ba1c220ffc9e8bc34f795aa8
Ezra <ezra.farrar@gmail.com>
2020-10-28 22:01:15 -0700

    replaced editorjs with react-ace

--- src/Components/Editor/index.js
+++ src/Components/Editor/index.js
@@ -1,29 +1,27 @@
 import React from 'react';
-import { useState, useEffect} from 'react';
+import { useState, useEffect } from 'react';
 import ReactDOM from 'react-dom';
-import {Editor, EditorState} from 'draft-js';
+import AceEditor from "react-ace";
 import './styles/index.css';
 
+import "ace-builds/src-noconflict/mode-java";
+import "ace-builds/src-noconflict/theme-github";
+
 function EditWindow() {
-  const [editorState, setEditorState] = useState(EditorState.createEmpty());
 
-  React.useEffect(() => {
-    focusEditor()
-  }, []);
-
-  const editor = React.useRef(null);
- 
-  function focusEditor() {
-    editor.current.focus();
+  function onChange(newValue) {
+    console.log("change", newValue);
   }
- 
- 
+
+
   return (
-    <div id="editor-wrapper" onClick={focusEditor}>
-      <Editor
-        ref={editor}
-        editorState={editorState}
-        onChange={editorState => setEditorState(editorState)}
+    <div id="editor-wrapper">
+      <AceEditor
+        mode="java"
+        theme="github"
+        onChange={onChange}
+        name="UNIQUE_ID_OF_DIV"
+        editorProps={{ $blockScrolling: true }}
       />
     </div>
   );
