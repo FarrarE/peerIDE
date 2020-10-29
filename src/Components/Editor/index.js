52269b15d21a6468c3b7055a3cb7f494cebe5295
Ezra <ezra.farrar@gmail.com>
2020-10-28 21:06:39 -0700

    Initial component boilerplate.

---
+++ src/Components/Editor/index.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import { useState, useEffect} from 'react';
+import ReactDOM from 'react-dom';
+import {Editor, EditorState} from 'draft-js';
+
+function EditWindow() {
+  const [editorState, setEditorState] = useState(EditorState.createEmpty());
+
+  React.useEffect(() => {
+    focusEditor()
+  }, []);
+
+  const editor = React.useRef(null);
+ 
+  function focusEditor() {
+    editor.current.focus();
+  }
+ 
+ 
+  return (
+    <div id="editor-wrapper" onClick={focusEditor}>
+      <Editor
+        ref={editor}
+        editorState={editorState}
+        onChange={editorState => setEditorState(editorState)}
+      />
+    </div>
+  );
+}
+
+export default EditWindow;
---
+++ src/Components/Header/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { useState, useEffect } from 'react';
+import ReactDOM from 'react-dom';
+
+function Header() {
+    useEffect(() => {
+    }, []);
+    return (
+        <div id="header-wrapper">
+
+        </div>
+    );
+}
+
+export default Header;
