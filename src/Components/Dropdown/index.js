5e949a1e814e0ea364c3d586fe433d11a5463d4c
Ezra <ezra.farrar@gmail.com>
2020-10-30 00:53:27 -0700

    Changed css colors to inherit from a centralized scss theme

--- src/Components/Dropdown/index.js
+++ src/Components/Dropdown/index.js
@@ -6,13 +6,20 @@ import './styles/index.css';
 function Header(props) {
     useEffect(() => {
     }, []);
+
+    const File =    <div className="dropdown-content">
+                        <div className="option"><p>Open File</p></div>
+                        <hr></hr>
+                        <div className="option"><p>Save As...</p></div>
+                        <hr></hr>
+                        <div className="option"><p>Preferences</p></div>
+                    </div>
+
     return (
         <div id="dropdown-wrapper">
             <div class="dropdown">
                 <span>{props.title}</span>
-                <div class="dropdown-content">
-                    <p>Work In Progress</p>
-                </div>
+                {File}
             </div>
         </div>
     );
